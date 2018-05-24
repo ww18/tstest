e5fab756b7de6abd5ae3b00f57e95825cc5b8e04
liwenwen <476848607@qq.com>
2018-05-24 22:06:13 +0800

    继承，多态，修饰符

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 /**
  * Created by ww on 2018/5/23.
  */
@@ -16,7 +26,72 @@ var Color;
 var c = Color.green; //获取下标
 var is = '123';
 var list = [1, 3, '232'];
+var list3 = [1, 3, '232'];
 function tell() {
     alert(Color[0]);
+    return 'hello';
 }
-tell();
+var add = function (n, a) {
+    alert(123);
+    return 'lall';
+};
+function hello(firstName, lastName) {
+    if (lastName === void 0) { lastName = 'wenwen'; }
+    return firstName + ' ' + lastName;
+}
+hello('hello');
+function change() {
+    var arg = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arg[_i] = arguments[_i];
+    }
+    return arg.join(' ');
+}
+var argumes = change('hello', 'world', 'lalal');
+document.getElementById('pid').innerHTML = argumes;
+//lambads就是箭头函数
+var people = {
+    name: ['hello', 'wenwen'],
+    sayName: function () {
+        var _this = this;
+        return function () {
+            var i = Math.floor(Math.random() * 2);
+            return 'sayname = ' + _this.name[i];
+        };
+    }
+};
+var newPeople = people.sayName();
+//alert(newPeople());
+function attr(nameorage) {
+    if (nameorage && typeof nameorage === 'string') {
+        alert(nameorage);
+    }
+    else {
+        alert('other');
+    }
+}
+//attr('string');
+//attr('string','lalll');
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person;
+}());
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(school) {
+        var _this = _super.call(this, 'ww', 100) || this;
+        _this.school = school;
+        _this.school = school;
+        return _this;
+    }
+    Teacher.prototype.saySchool = function () {
+        alert(this.name + this.age + this.school);
+    };
+    return Teacher;
+}(Person));
+var teacher = new Teacher();
+teacher.school = 'yid';
+teacher.saySchool();
--- index.ts
+++ index.ts
@@ -6,14 +6,73 @@ var num:number = 10;
 var str:string = '10';
 var list1:number[] = [1,2,3];
 var list2:Array<string> = ['iwen','ime'];
-enum Color {red, green, blue};//还可以修改下标
+enum Color {red, green, blue}; //还可以修改下标
 var c:Color = Color.green; //获取下标
 var is:any = '123';
 var list:any[] = [1,3,'232'];
-var list1:any = [1,3,'232'];
+var list3:any[] = [1,3,'232'];
 
 function tell():string{
     alert(Color[0]);
     return 'hello';
 }
-tell();
+var add:(name:string,age:number)=>string = function(n:string,a:number):string{
+    alert(123);
+    return 'lall';
+}
+
+function hello(firstName:string,lastName:string='wenwen'){
+    return firstName + ' ' + lastName;
+}
+hello('hello');
+function change(...arg:string[]){
+    return arg.join(' ');
+}
+var argumes = change('hello','world','lalal');
+document.getElementById('pid').innerHTML = argumes;
+
+//lambads就是箭头函数
+var people = {
+    name: ['hello','wenwen'],
+    sayName: function(){
+        return ()=>{
+            var i = Math.floor(Math.random()*2);
+            return 'sayname = ' + this.name[i];
+        }
+    }
+}
+var newPeople = people.sayName();
+//alert(newPeople());
+
+function attr(nameorage:any):any{
+    if(nameorage && typeof nameorage === 'string'){
+        alert(nameorage);
+    }else{
+        alert('other');
+    }
+}
+
+//attr('string');
+//attr('string','lalll');
+
+class Person{
+    name:string;
+    age:number;
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+}
+class Teacher extends Person{
+    school: string;
+    constructor(school?:string){
+        super('ww',100);
+        this.school = school;
+    }
+    saySchool(){
+        alert(this.name + this.age + this.school);
+    }
+}
+var teacher = new Teacher();
+teacher.school = 'yid';
+teacher.saySchool();
